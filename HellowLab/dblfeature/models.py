2f5926dba0a1ac041ad406a5a9482f83fa29aadf
Kevin Mengarelli <mango23322@gmail.com>
2024-08-10 12:36:35 -0500

    add MovieResults post, update movieresult model

--- HellowLab/dblfeature/models.py
+++ HellowLab/dblfeature/models.py
@@ -4,13 +4,14 @@ from django.conf import settings
 #   Movie results track the result of each swipe a user makes
 class MovieResult(models.Model):
   id = models.AutoField(primary_key=True)
-  user = models.ForeignKey(settings.Auth_USER_MODEL)
-  imdbResult = models.CharField(max_length=30)
-  swipeResult = models.BooleanField()
+  user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+  tmdb_id = models.CharField(max_length=30)
+  movie_name = models.CharField(max_length=100)
+  liked = models.BooleanField()
   swipeDate = models.DateTimeField(auto_now_add=True)
   watched = models.BooleanField(default=False)
   myRanking = models.IntegerField(null=True, blank=True)
   myComments = models.CharField(max_length=500, null=True, blank=True)
 
   def __str__(self):
-    return self.name
+    return (f"{self.tmdb_id}")
--- HellowLab/dblfeature/views.py
+++ HellowLab/dblfeature/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from .serializers import * # import all of my serializers
+from .models import * # import all of my models
 
-# Create your views here.
+# update movie result
+class MovieResultView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = self.request.user
+        print("user: ", user)
+        serializer = MovieResultSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=self.request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
