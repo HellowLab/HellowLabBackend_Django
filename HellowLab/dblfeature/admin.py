cb4dbff5143d69835ebbc6b9c6fa520c6001ea81
Kevin Mengarelli <mango23322@gmail.com>
2024-08-22 19:57:45 -0500

    add tmdb index

--- HellowLab/dblfeature/admin.py
+++ HellowLab/dblfeature/admin.py
@@ -5,3 +5,4 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(MovieResult)
+admin.site.register(TmdbIndex)
--- HellowLab/dblfeature/urls.py
+++ HellowLab/dblfeature/urls.py
@@ -7,14 +7,6 @@ from .views import *
 urlpatterns = [
     # update movie result
     path('movieresult/', MovieResultView.as_view(), name='movie_result'),
-    path('singlemovieresult/', SingleMovieView.as_view(), name='single_movie_result')
-    # path('update_user_data/', update_user_data, name='update_user_data'),
-    # path('myuser', get_my_user, name='get_my_user'),
-
-    # path('alltags/', tag_list, name='all-tagslist'),
-    # path('tagitems/<int:item_id>/', ItemDetailView.as_view(), name='item-detail'),
-    # path('tagitems/csvupload/', import_csv, name='tag_item_upload_csv'),
-
-    # path('newcomment/', create_comment, name='create_comment'),
-    # path('getcomments/<int:tagID>/', get_comment, name="get_comment_for_tag"),
+    path('singlemovieresult/', SingleMovieView.as_view(), name='single_movie_result'),
+    path('tmdbindex/', TmdbIndexView.as_view(), name='tmdb_index')
 ]
