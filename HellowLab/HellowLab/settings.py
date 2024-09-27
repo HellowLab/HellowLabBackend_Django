d445e60d3e2d34a835c51eeb18a4637d0bdb0006
Kevin Mengarelli <mango23322@gmail.com>
2024-09-26 20:09:54 -0500

    update postgres info

--- HellowLab/HellowLab/settings.py
+++ HellowLab/HellowLab/settings.py
@@ -103,11 +103,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv('POSTGRES_DB', 'postgres'),
-            "USER": os.getenv('POSTGRES_USER', 'postgres'),
-            "PASSWORD": os.getenv('POSTGRES_PASSWORD', 'postgres'),
-            "HOST": os.getenv('POSTGRES_HOST', 'db'),
-            "PORT": os.getenv('POSTGRES_PORT', '5432'),
+            "NAME": os.getenv('DJANGO_DB_NAME', 'hellowlab_db'),
+            "USER": os.getenv('DJANGO_DB_USER', 'admin'),
+            "PASSWORD": os.getenv('DJANGO_DB_PASSWORD', 'password'),
+            "HOST": os.getenv('DJANGO_DB_HOST', 'db'),
+            "PORT": os.getenv('DJANGO_DB_PORT', '5432'),
         }
     }
 
